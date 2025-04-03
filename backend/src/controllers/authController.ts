3257d1586f97c4e27407485da9f95e76684b2133
unknown <chikviladze555@yahoo.com>
2025-04-03 19:40:32 +0400

    feat: add DockerFile

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import * as authService from '../services/authService';
 import { signupSchema } from '../../../shared/schema/schemas';
 import prisma from '../prisma/prisma';
